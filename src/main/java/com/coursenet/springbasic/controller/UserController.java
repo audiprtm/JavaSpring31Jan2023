beb3074534fda0e5eb93710cfe656a9faf458209
DESKTOP-JU3KB60\ASUS <anggawakharisma@gmail.com>
2023-02-07 20:32:22 +0700

    update fitur

---
+++ src/main/java/com/coursenet/springbasic/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.coursenet.springbasic.controller;
+
+import com.coursenet.springbasic.dto.UserRequestDTO;
+import com.coursenet.springbasic.dto.UserResponseDTO;
+import com.coursenet.springbasic.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<UserResponseDTO> createUsers(@RequestBody UserRequestDTO userRequest){
+        log.info(userRequest.toString());
+        return userService.createUser(userRequest);
+    }
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponseDTO>> getUsers(
+            @RequestParam(value="id", required=false) Long id,
+            @RequestParam(value="name", required=false) String name
+    ){
+        return userService.getUsers(id,name);
+
+    }
+}
